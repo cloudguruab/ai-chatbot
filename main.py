416d2f72efa9044d9b0b56e760cebe42b6d382b1
cloudguruab <ap.brown011@gmail.com>
2021-03-05 10:08:30 -0500

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+#! usr/bin/python3 
+
+import nltk
+import numpy
+import tflearn
+import random
+import json
+# import tensorflow
+from nltk.stem.lancaster import LancasterStemmer
+
+stemmer = LancasterStemmer()
+
+# issue with tensor flow install 
---
+++ readme.md
@@ -0,0 +1,6 @@
+# AI Chatbot
+It's sort of intelligent. 
+
+Here I use python to create a simple chatbot encompassing neural networks and deep learning. 
+
+## View: 
